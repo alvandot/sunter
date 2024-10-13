552e38ac2d771bf45b167a109e1de10779ff7564
alvan <alvan.doc@gmai.com>
2024-10-13 20:47:18 +0700

    first commit

---
+++ resources/views/livewire/welcome.blade.php
@@ -0,0 +1,45 @@
+<div>
+    <!-- HEADER -->
+    <x-header title="Hello" separator progress-indicator>
+        <x-slot:middle class="!justify-end">
+            <x-input placeholder="Search..." wire:model.live.debounce="search" clearable icon="o-magnifying-glass" />
+        </x-slot:middle>
+        <x-slot:actions>
+            <x-button label="Filters" @click="$wire.drawer = true" responsive icon="o-funnel" class="btn-primary" />
+        </x-slot:actions>
+    </x-header>
+
+    <div class="grid grid-cols-2 gap-4 mb-4 ">
+
+        <x-stat
+            title="Total Sesi"
+            description="Hari ini"
+            value="4"
+            icon="o-calendar-days" />
+
+        <x-stat
+            title="Tingkat Kehadiran"
+            description="Rata-rata"
+            value="85%"
+            icon="o-chart-bar"
+            class="text-purple-500" />
+    </div>
+    <!-- TABLE  -->
+    <x-card title="Jadwal Hari ini" subtitle="{{ now()->format('l, d F Y') }}" shadow separator>
+        <x-table :headers="$headers" :rows="$users" with-pagination :sort-by="$sortBy">
+            @scope('actions', $user)
+            <x-button icon="o-trash" wire:click="delete({{ $user['id'] }})" spinner class="btn-ghost btn-sm text-red-500" />
+            @endscope
+        </x-table>
+    </x-card>
+
+    <!-- FILTER DRAWER -->
+    <x-drawer wire:model="drawer" title="Filters" right separator with-close-button class="lg:w-1/3">
+        <x-input placeholder="Search..." wire:model.live.debounce="search" icon="o-magnifying-glass" @keydown.enter="$wire.drawer = false" />
+
+        <x-slot:actions>
+            <x-button label="Reset" icon="o-x-mark" wire:click="clear" spinner />
+            <x-button label="Done" icon="o-check" class="btn-primary" @click="$wire.drawer = false" />
+        </x-slot:actions>
+    </x-drawer>
+</div>
