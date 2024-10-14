43044f55ec9b940425f9eb03ebf6393d75ede653
alvan <alvan.doc@gmai.com>
2024-10-14 14:19:28 +0700

    fix route

--- routes/web.php
+++ routes/web.php
@@ -18,12 +18,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware(['auth'])->group(function () {
-    Route::livewire('/', Welcome::class)->name('home');
-    Route::livewire('/siswa', SiswaIndex::class)->name('siswa.index');
+    Route::get('/', Welcome::class)->name('home');
+    Route::get('/siswa', SiswaIndex::class)->name('siswa.index');
 });
 
-Route::livewire('/login', Login::class)->name('login');
-Route::livewire('/logout', function () {
+Route::get('/login', Login::class)->name('login');
+Route::get('/logout', function () {
     Auth::logout();
     session()->invalidate();
     session()->regenerateToken();
