5fdbb5e8db581f1a2ba8aacf74121c7fb3218f9f
alvandot <alfanfauzan2505@gmail.com>
2024-10-20 17:30:15 +0700

    Update index.blade.php
    fix breadcrumb

--- resources/views/livewire/tentor/index.blade.php
+++ resources/views/livewire/tentor/index.blade.php
@@ -8,6 +8,9 @@
                         d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z" />
                 </svg>
             </li>
+            <li>
+                <span class="text-gray-500">List Tentor</span>
+            </li>
         </ol>
     </nav>
 
@@ -20,7 +23,7 @@
         </x-slot:actions>
     </x-header>
 
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 cursor-pointer ">
         @foreach ($tentors as $tentor)
             <div class="bg-white rounded-lg shadow-md overflow-hidden transition-all duration-300 hover:shadow-lg hover:scale-105"
                 wire:click="detailTentor({{ $tentor->id }})">
